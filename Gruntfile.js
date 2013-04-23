13765f8794b539e4c753b4e08ce6e9a4da971558
Bartvds <bartvanderschoor@gmail.com>
2013-04-23 16:55:53 +0200

    first files

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+/*
+ * grunt-deserve
+ * https://github.com/Bartvds/grunt-deserve
+ *
+ * Copyright (c) 2013 Bart van der Schoor
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			}
+		},
+
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
+
+		// Configuration to be run (and then tested).
+		deserve: {
+			default: {
+			},
+			custom: {
+				options: {
+					port: 8071,
+					tinylrPort: 32198,
+					hostname: '127.0.0.1',
+					base: 'test/fixtures/base_custom'
+				}
+			},
+			keepalive: {
+				options: {
+					keepalive: true,
+					base: 'test/fixtures/base_custom'
+				}
+			}
+		},
+		deserve_reload: {
+			default: {
+			},
+			custom: {
+				options: {
+					tinylrPort: 32198,
+					hostname: '127.0.0.1'
+				}
+			}
+		},
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
+
+	});
+
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'deserve:default', 'deserve:custom', 'nodeunit']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
+
+	grunt.registerTask('edit_01', ['deserve:keepalive']);
+	grunt.registerTask('edit_02', ['deserve-reload:default']);
+
+};
---
+++ tasks/deserve.js
@@ -0,0 +1,167 @@
+/*
+ * Bart van der Schoor
+ * Licensed under the MIT license.
+ *
+ * butchered from grunt-contrib-connect & livereload
+ */
+
+'use strict';
+
+var url = require('url');
+var path = require('path');
+
+// reworked from grunt-livereload
+var getSnippet = function (tinylrPort) {
+	return [
+		'<!-- livereload snippet -->',
+		'<script>',
+		'var js = document.createElement("script");',
+		'js.src = "http://" + (location.host || "localhost").split(":")[0]+ ":' + tinylrPort + '/livereload.js?snipver=1";',
+		'document.getElementsByTagName("head")[0].appendChild(js);',
+		'</script>',
+		''
+	].join('\n');
+};
+
+//
+// This function returns a connect middleware that will insert a snippet
+// of JavaScript needed to connect to the livereload server
+var getLiveReloadMiddleware = function (tinylrPort) {
+
+
+	// code from grunt-livereload
+	return function (req, res, next) {
+		var write = res.write;
+
+		var filepath = url.parse(req.url).pathname;
+		filepath = filepath.slice(-1) === '/' ? filepath + 'index.html' : filepath;
+
+		if (path.extname(filepath) !== '.html') {
+			return next();
+		}
+
+		res.write = function (string, encoding) {
+			var body = string instanceof Buffer ? string.toString() : string;
+
+			body = body.replace(/<\/body>/, function (w) {
+				return getSnippet(tinylrPort) + w;
+			});
+
+			if (string instanceof Buffer) {
+				string = new Buffer(body);
+			} else {
+				string = body;
+			}
+
+			if (!this.headerSent) {
+				this.setHeader('content-length', Buffer.byteLength(body));
+				this._implicitHeader();
+			}
+
+			write.call(res, string, encoding);
+		};
+
+		next();
+	};
+};
+
+module.exports = function (grunt) {
+
+	var express = require('express');
+	var tinylr = require('tiny-lr');
+	var request = require('request');
+	var util = require('util');
+
+	//TODO keep map of servers? poll for servers?
+	var tinylrPort = 35729;
+
+	grunt.registerMultiTask('deserve', 'Start a dev server.', function () {
+
+		// Merge task-specific options with these defaults.
+		var options = this.options({
+			port: 8000,
+			hostname: 'localhost',
+			base: '.',
+			debug: true,
+			tinylrPort: tinylrPort,
+			keepalive: false
+		});
+
+		// Connect requires the base path to be absolute.
+		options.base = path.resolve(options.base);
+
+		//grunt.log.writeln(util.inspect(options));
+
+		var done = this.async();
+		var setupApp = function () {
+			var app = express();
+
+			//app.set('view engine', 'jade');
+			//app.set('views', __dirname + '/views');
+			if (grunt.option('debug') || options.debug) {
+				app.use(express.logger('dev'));
+			}
+			//TODO merge dev api's services
+			/*
+			 app.use(express.cookieParser());
+			 app.use(express.bodyParser());
+			 app.use(express.query());
+			 app.use(express.methodOverride());*/
+			app.use(getLiveReloadMiddleware(options.tinylrPort));
+			app.use(express.static(options.base));
+			//app.use(app.router);
+
+			grunt.log.writeln('Starting connect web server on %s:%s.', options.hostname, options.port);
+			grunt.log.writeln('Serving files from %s', options.base);
+
+			app.listen(options.port, options.hostname);
+
+			app.on('error', function (err) {
+				if (err.code === 'EADDRINUSE') {
+					grunt.fatal('Port %d is already in use by another process.', options.port);
+				} else {
+					grunt.fatal(err);
+				}
+			});
+
+			if (this.flags.keepalive || options.keepalive) {
+
+				grunt.log.write('Waiting forever...\n');
+			}
+			else {
+				done();
+			}
+		};
+
+		//TODO add polling of port
+
+		var that = this;
+		var tinylrServer = tinylr();
+		tinylrServer.listen(options.tinylrPort, function () {
+			grunt.log.writeln('tiny-lr listening on %d', options.tinylrPort);
+			setupApp.call(that);
+		});
+	});
+
+	grunt.registerMultiTask('deserve_reload', 'Trigger reload on connected browsers', function () {
+		var options = this.options({
+			hostname: 'localhost',
+			debug: true,
+			tinylrPort: tinylrPort
+		});
+		var done = this.async();
+		request.get('http://' + options.hostname + ':' + options.tinylrPort + '/changed', function (error, response, body) {
+			if (error) {
+				grunt.log.write('error!' + error);
+			}
+			else {
+				grunt.log.write('reloaded ' + response.statusCode);
+			}
+			if (options.debug && body) {
+				grunt.log.write(body);
+			}
+			done();
+		});
+	});
+
+};
